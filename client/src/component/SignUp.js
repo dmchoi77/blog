1c1d220e03df71195ccb9c7c6cddc4e0560cab85
dmchoi <david5760@naver.com>
2021-08-19 17:03:16 +0900

    회원가입
    -DOM에서 useState를 이용한 상태 관리로 코드 수정

--- client/src/component/SignUp.js
+++ client/src/component/SignUp.js
@@ -1,16 +1,34 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 
 function SignUp() {
 
+    const [name, setName] = useState('');
+    const [id, setId] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [pwdCheck, setPwdCheck] = useState('');
+
+    const handleInputName = (e) => {
+        setName(e.target.value.trim());
+    }
+    const handleInputId = (e) => {
+        setId(e.target.value.trim());
+    }
+    const handleInputPwd = (e) => {
+        setPwd(e.target.value.trim());
+    }
+    const handleInputPwdCheck = (e) => {
+        setPwdCheck(e.target.value.trim());
+    }
+
     const history = useHistory();
     const _signUp = () => {
-        const userName = document.getElementsByName('signup_name')[0].value.trim();
-        const id = document.getElementsByName('signup_id')[0].value.trim();
-        const password = document.getElementsByName('signup_password')[0].value.trim();
-        const psw_check = document.getElementsByName('signup_pswCheck')[0].value.trim();
+        // const userName = document.getElementsByName('signup_name')[0].value.trim();
+        // const id = document.getElementsByName('signup_id')[0].value.trim();
+        // const password = document.getElementsByName('signup_password')[0].value.trim();
+        // const psw_check = document.getElementsByName('signup_pswCheck')[0].value.trim();
 
         const eng_check = /^[a-z]+[a-z0-9]{5,19}$/g;
         const pw_check = /^[a-z]+[a-z0-9]{5,19}$/g;
@@ -19,19 +37,19 @@ function SignUp() {
             return alert('아이디는 영문자로 시작하는 6~20자여야만 합니다.')
         }
 
-        if (!pw_check.test(password)) {
+        if (!pw_check.test(pwd)) {
             return alert('비밀번호는 영문자로 시작하는 6~20자여야만 합니다.')
 
-        } else if (password !== psw_check) {
+        } else if (pwd !== pwdCheck) {
             return alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.')
         }
 
         (() => {
             axios.post('http://localhost:8000/api/signup', {
-                name: userName,
+                name: name,
                 id: id,
-                password: password,
-                psw_check: psw_check
+                password: pwd,
+                psw_check: pwdCheck
             }).then((res) => {
                 if (res.data === "ER_DUP_ENTRY") return alert("이미 가입되어 있는 아이디 입니다.");
                 else if (res.data === "complete") {
@@ -47,19 +65,19 @@ function SignUp() {
         <div id="container">
             <h1>회원가입</h1>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control type='text' maxLength='10' name='signup_name' placeholder="이름" />
+                <Form.Control type='text' maxLength='10' name='signup_name' placeholder="이름" onChange={handleInputName} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Control type='text' maxLength='20' name='signup_id' placeholder="아이디" />
+                <Form.Control type='text' maxLength='20' name='signup_id' placeholder="아이디" onChange={handleInputId} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Control type='password' maxLength='15' name='signup_password' placeholder="비밀번호" />
+                <Form.Control type='password' maxLength='15' name='signup_password' placeholder="비밀번호" onChange={handleInputPwd} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Control type='password' maxLength='15' name='signup_pswCheck' placeholder="비밀번호 확인" />
+                <Form.Control type='password' maxLength='15' name='signup_pswCheck' placeholder="비밀번호 확인" onChange={handleInputPwdCheck} />
             </Form.Group>
             <Button variant="primary" type='button' onClick={_signUp}>
                 회원가입

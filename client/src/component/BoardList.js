8f970e1a8d642d423784e9ceb7c7750b20f48e20
dmchoi <david5760@naver.com>
2021-08-26 17:35:01 +0900

    useEffect와 axios 분리

--- client/src/component/BoardList.js
+++ client/src/component/BoardList.js
@@ -23,23 +23,23 @@ function BoardList() {
     //검색어 상태 관리
     const [search, setSearch] = useState('');
 
-    useEffect(() => {
-        let isComponentMounted = true;
-        axios.get('http://localhost:8000/api/get')
-            .then((response) => {
-                let data = response.data.reverse();
-                if (isComponentMounted) {
-                    setList({
-                        data,
-                        pageSize: 10,
-                        currentPage: 1,
-                        searchKeyword: ''
-                    });
-                }
-            })
-        return () => {
-            isComponentMounted = false
+    const fetchData = async () => {
+        try {           
+            const result = await axios.get('http://localhost:8000/api/get')
+            let data = result.data.reverse();
+            setList({
+                data,
+                pageSize: 10,
+                currentPage: 1,
+                searchKeyword: ''
+            });
         }
+        catch {
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
     }, [])
 
     //페이징

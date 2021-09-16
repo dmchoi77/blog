f2c1b7d1e5a8686e2e56f114b311ccdefc1b2436
dmchoi <david5760@naver.com>
2021-09-16 20:55:25 +0900

    fix - PayloadTooLargeError 수정

--- server/index.js
+++ server/index.js
@@ -12,19 +12,23 @@ const view = require('./router/view');
 const userInform = require('./router/userInform');
 
 app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({
+    limit: "50mb",
+}));
+app.use(bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true
+}));
 
 
-app.use('/',boardManage);
-app.use('/',boardList);
-app.use('/',signUp);
-app.use('/',view);
-app.use('/',userInform);
+app.use('/', boardManage);
+app.use('/', boardList);
+app.use('/', signUp);
+app.use('/', view);
+app.use('/', userInform);
 
 app.listen(PORT, () => {
     console.log(`running on PORT ${PORT}`);
 });
 
-    
- 
+

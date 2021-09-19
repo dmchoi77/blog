e5cc519750049464891465635e6804e2c097f428
dmchoi <david5760@naver.com>
2021-09-19 21:56:06 +0900

    Fix - 헤더 고정

--- client/src/Main.js
+++ client/src/Main.js
@@ -2,7 +2,7 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { Link, Route } from 'react-router-dom';
-import { Nav, Navbar, Container, Row, Col, Card } from 'react-bootstrap';
+import { Nav, Navbar, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 import SignUp from './components/pages/SignUp';
 import Login from './components/pages/Login';
@@ -11,6 +11,7 @@ import BoardWrite from './components/pages/BoardWrite';
 import BoardModify from './components/pages/BoardModify';
 import BoardList from './components/pages/BoardList';
 import View from './components/pages/View';
+import LeftNav from './components/modules/LeftNav';
 
 function Main() {
   // 로그인 상태 관리
@@ -57,51 +58,46 @@ function Main() {
 
   else
     return (
-      <div>
-        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
-          <Container>
-            <Navbar.Brand>
-              <Link to={"/home"} className="home-link">
-                dmchoi
+      <Container>
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" style={{ zIndex: "999" }}>
+          <Navbar.Brand>
+            <Link to={"/home"} className="home-link">
+              dmchoi
                 </Link>
-            </Navbar.Brand>
-            {isLogin ?
-              <Nav.Link onClick={onLogout}>
-                Log out
+          </Navbar.Brand>
+          {isLogin ?
+            <Nav.Link onClick={onLogout}>
+              Log out
                 </Nav.Link>
-              : <Nav.Link onClick={onLogin}>
-                Log In
+            : <Nav.Link onClick={onLogin}>
+              Log In
               </Nav.Link>
-            }
-          </Container>
+          }
         </Navbar>
-        <Container>
-          <Row>
-            <Col className="category">
-              <Card.Body>
-                <Card.Header as="h5">Categories</Card.Header>
-                <Card.Text>
-                  <li><Link to={"/board/list"} className="link">Board</Link></li>
-                </Card.Text>
-              </Card.Body>
-            </Col>
-            <Col xs={10}>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/board/newpost" component={BoardWrite} />
-              <Route exact path="/home" component={Home} />
-              <Route exact path="/board/list" component={BoardList} />
-              <Route exact path="/board/view/:data" component={View} />
-              <Route exact path="/board/modify/:data" component={BoardModify} />
-            </Col>
-          </Row>
-        </Container>
+        <Contents>
+          <Col xs={1}>
+            <LeftNav style={{ padding: "3rem" }}>
+            </LeftNav>
+          </Col>
+          <Col xs={6}>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/board/newpost" component={BoardWrite} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/board/list" component={BoardList} />
+            <Route exact path="/board/view/:data" component={View} />
+            <Route exact path="/board/modify/:data" component={BoardModify} />
+          </Col>
+        </Contents>
         <Footer>
           <div>Contact - minminnn11@daum.net</div>
           <div className="cr">Copyright©dmchoi 2021</div>
         </Footer>
-      </div >
+      </Container >
     )
 }
+const Container = styled.div`
+  width : 100%;
+`
 
 const Footer = styled.div`
   width: 100%;
@@ -114,5 +110,13 @@ const Footer = styled.div`
   color: white;
   font-weight: 600;
 `
+
+const Contents = styled.div`
+  width : 100%;
+  padding-top : 3rem;
+  display:flex;
+  justify-content: center;
+`
+
 
 export default Main;

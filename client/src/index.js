d76953ff231f5d8bdfabcc8d1bf4028237cfc2bf
dmchoi <david5760@naver.com>
2021-11-26 10:07:36 +0900

    Fix - 경로 수정

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { applyMiddleware,createStore } from 'redux'
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
-import Reducer from './_reducer';
+import Reducer from './reducer';
 
 const createStoreWitdhMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 

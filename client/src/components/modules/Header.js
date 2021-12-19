20daeffc8abc0c6adeff914500a5cf8d95360e20
dmchoi <david5760@naver.com>
2021-12-19 13:51:58 +0900

    Fixed - Header 수정

--- client/src/components/modules/Header.js
+++ client/src/components/modules/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link, useHistory } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 import { List } from './LeftNav';
 import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../../actions/user_action";
@@ -33,9 +33,47 @@ function Header() {
         setToggle(!toggle);
     }
 
+    const onSearch = () => {
+        history.push('/search');
+    }
+
     return (
         <Container>
-            <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" style={{ zIndex: "999", width: "1340px", margin: "0 auto", position: "fixed", top: 0, left: 0, right: 0 }}>
+            <Nav>
+                <div>
+                    <Link to={"/"}>
+                        dmchoi
+                    </Link>
+                </div>
+                <ul style={{ display: "flex", alignItems: "center", marginTop: "revert", cursor: "pointer", padding: 0 }}>
+                    <li>
+                        <img src="/img/search.png" style={{ width: "25px", height: "25px" }} onClick={onSearch} />
+                    </li>
+                    {!user.userData.isAuth ?
+                        <li style={{
+                            background: "#e05194",
+                            borderRadius: "100px",
+                            padding: "4px 15px",
+                            color: "#ffff"
+                        }} onClick={onLogin}>
+                            로그인
+                            </li> :
+                        <li style={{
+                            background: "#e05194",
+                            borderRadius: "100px",
+                            padding: "4px 15px",
+                            color: "#ffff"
+                        }} onClick={onLogout}>
+                            로그아웃
+                </li>
+                    }
+
+                </ul>
+            </Nav>
+
+
+
+            {/* <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" style={{ zIndex: "999", width: "1340px", margin: "0 auto", position: "fixed", top: 0, left: 0, right: 0 }}>
                 <HeaderToggle className="Header-Toggle" onClick={toggleMenu}>
                     <Button src="/img/menu.png"></Button>
                     <div>
@@ -67,7 +105,7 @@ function Header() {
                         Log out
                    </Nav.Link>
                 }
-            </Navbar>
+            </Navbar> */}
         </Container >
     )
 }
@@ -76,37 +114,46 @@ function Header() {
 const Container = styled.div`
     width : 100%;
     height : 62px;
-    background-color: #0d6efd;
+    background-color: #ffff;
     position : fixed;
     z-index : 999;
-
-    @media(max-width : 811px) {
-        height : 1vw;   
-    }
+    box-shadow: 0px 0px 4px grey;
+    display : flex;
+    align-items : center;
 `
 
-const HeaderToggle = styled.div`
-    position : fixed;
-    z-index : 1001;
-    left : -5px;
+const Nav = styled.nav`
+    width : 1000px;
+    display : flex;
+    margin : 0 auto;
+    justify-content : space-between;
+    align-items : center;
+    padding : 1rem 2rem;
+
 `
 
-const Button = styled.img`
+// const HeaderToggle = styled.div`
+//     position : fixed;
+//     z-index : 1001;
+//     left : -5px;
+// `
 
-    @media(min-width : 812px) {
-        display : none;
-    }
+// const Button = styled.img`
 
-    @media(max-width : 811px){
-        width : 19px;
-        height : 19px;
-        position : absolute;
-        top : -8px;
-        left : 15px;
-        border : none;
-        background : none;
-        z-index : 1000;
-    }
-`
+//     @media(min-width : 812px) {
+//         display : none;
+//     }
+
+//     @media(max-width : 811px){
+//         width : 19px;
+//         height : 19px;
+//         position : absolute;
+//         top : -8px;
+//         left : 15px;
+//         border : none;
+//         background : none;
+//         z-index : 1000;
+//     }
+// `
 
 export default Header;

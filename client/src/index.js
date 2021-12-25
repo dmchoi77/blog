8f48c85f09e4e70c0a5e122aeab1c3824b0e233d
dmchoi <david5760@naver.com>
2021-12-25 18:21:45 +0900

    Rollback

--- client/src/index.js
+++ client/src/index.js
@@ -1,40 +1,30 @@
-import React from 'react';
-import { render, hydrate } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import Main from './Main';
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux'
-import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk';
-import Reducer from './reducer';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import Main from "./Main";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./reducer";
 
-const createStoreWitdhMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
-const rootElement = document.getElementById('root');
+const createStoreWitdhMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
-
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <BrowserRouter>
-      <Provider
-        store={createStoreWitdhMiddleware(Reducer,
-          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )}>
-        <Main />
-      </Provider>
-    </BrowserRouter>,
-    rootElement
-  );
-} else {
-  render(
-    <BrowserRouter>
-      <Provider
-        store={createStoreWitdhMiddleware(Reducer,
-          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )}>
-        <Main />
-      </Provider>
-    </BrowserRouter>
-    , rootElement
-  );
-}
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider
+      store={createStoreWitdhMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <Main />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);

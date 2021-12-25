ebdcc9602318734d6ad29cae8fe80bc64790ce26
dmchoi <david5760@naver.com>
2021-12-25 18:43:10 +0900

    Update - CSS 수정

--- client/src/components/pages/Board/BoardDetail/Sections/ReplyComment.js
+++ client/src/components/pages/Board/BoardDetail/Sections/ReplyComment.js
@@ -1,52 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import SingleComment from './SingleComment';
+import React, { useEffect, useState } from "react";
+import SingleComment from "./SingleComment";
 
 function ReplyComment(props) {
-    const [childCommentNumber, setChildCommentNumber] = useState(0)
-    const [openReply, setOpenReply] = useState(false)
-    const isReply = true;
+  const [childCommentNumber, setChildCommentNumber] = useState(0);
+  const [openReply, setOpenReply] = useState(false);
+  const isReply = true;
 
-    useEffect(() => {
-        let commentNumber = 0;
+  useEffect(() => {
+    let commentNumber = 0;
 
-        props.commentList.map((comment, index) => {
-            if (comment.responseTo === props.parentCommentId) {
-                commentNumber++;
-            }
-        })
+    props.commentList.map((comment, index) => {
+      if (comment.responseTo === props.parentCommentId) {
+        commentNumber++;
+      }
+    });
 
-        setChildCommentNumber(commentNumber)
-    }, [props.commentList, props.parentCommentId])
+    setChildCommentNumber(commentNumber);
+  }, [props.commentList, props.parentCommentId]);
 
-    const renderReplyComment = (parentCommentId) =>
+  const renderReplyComment = (parentCommentId) =>
+    props.commentList.map((comment, index) => (
+      <>
+        {comment.responseTo === parentCommentId && (
+          <div
+            style={{ width: "100%", marginLeft: "40px", marginBottom: "15px" }}
+          >
+            <SingleComment
+              isReply={isReply}
+              comment={comment}
+              idx={props.idx}
+              commentList={props.commentList}
+              refreshFunction={props.refreshFunction}
+            />
+          </div>
+        )}
+      </>
+    ));
 
-        props.commentList.map((comment, index) => (
-            <>
-                {
-                    comment.responseTo === parentCommentId &&
-                    <div style={{ width: '100%', marginLeft: '40px', marginBottom: '15px' }}>
-                        <SingleComment isReply={isReply} comment={comment} idx={props.idx} commentList={props.commentList} refreshFunction={props.refreshFunction} />
-                    </div>
-                }
-            </>
-        ))
+  const onHandleChange = () => {
+    setOpenReply(!openReply);
+  };
 
-    const onHandleChange = () => {
-        setOpenReply(!openReply)
-    }
-
-    return (
-        <div>
-            { childCommentNumber > 0 &&
-                <p style={{ fontSize: '14px', margin: 0, margin: '10px' }} onClick={onHandleChange}>
-                    더보기 ({childCommentNumber})
-                </p>
-            }
-            {openReply &&
-                renderReplyComment(props.parentCommentId)
-            }
-        </div>
-    )
+  return (
+    <div>
+      {childCommentNumber > 0 && (
+        <p
+          style={{ fontSize: "14px", margin: "10px" }}
+          onClick={onHandleChange}
+        >
+          더보기 ({childCommentNumber})
+        </p>
+      )}
+      {openReply && renderReplyComment(props.parentCommentId)}
+    </div>
+  );
 }
 
-export default ReplyComment
+export default ReplyComment;

7e7b76f964f3aa0bc088cfb9f43a0684d28807c5
dmchoi <david5760@naver.com>
2021-12-25 18:42:52 +0900

    Update - useEffect dependency 추가

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,38 +1,34 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
-import { auth } from '../actions/user_action';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { auth } from "../actions/user_action";
 
 export default function (SpecificComponent, option, adminRoute = null) {
+  //option
+  //null : 아무나 접근 가능한 페이지
+  //true : 로그인한 유저만 접근 가능한 페이지
+  //false : 로그인한 유저는 접근 불가능한 페이지
 
-    //option
-    //null : 아무나 접근 가능한 페이지
-    //true : 로그인한 유저만 접근 가능한 페이지
-    //false : 로그인한 유저는 접근 불가능한 페이지
+  function AuthenticationCheck(props) {
+    const dispatch = useDispatch();
 
-    function AuthenticationCheck(props) {
+    useEffect(() => {
+      dispatch(auth()).then((response) => {
+        //console.log(response)
+        if (!response.payload.isAuth) {
+          if (option) {
+            props.history.push("/login");
+          }
+        } else {
+          if (adminRoute && !response.payload.isAdmin) {
+            props.history.push("/");
+          } else {
+            if (option === false) props.history.push("/");
+          }
+        }
+      });
+    }, [dispatch, props.history.push, props.history]);
 
-        const dispatch = useDispatch();
-
-        useEffect(() => {
-            dispatch(auth()).then(response => {
-                //console.log(response)
-                if (!response.payload.isAuth) {
-                    if (option) {
-                        props.history.push('/login');
-                    }
-                } else {
-                    if (adminRoute && !response.payload.isAdmin) {
-                        props.history.push('/');
-                    } else {
-                        if (option === false) props.history.push('/');
-                    }
-                }
-            })
-        }, [dispatch, props.history.push])
-
-        return <SpecificComponent {...props} />;
-
-    }
-    return AuthenticationCheck
-
+    return <SpecificComponent {...props} />;
+  }
+  return AuthenticationCheck;
 }

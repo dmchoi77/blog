d41b00329724323945f30baedbb80c2183f0ec2a
dmchoi <david5760@naver.com>
2021-12-30 01:33:33 +0900

    Fix - token 가져오는 방법 수정

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,26 +1,24 @@
-const { User } = require('../models/User')
+const { User } = require("../models/User");
 
 let auth = (req, res, next) => {
+  //클라이언트 쿠기에서 토큰을 가져온다
 
-    //인증 처리를 하는 곳 
+  const token = req.headers["x_auth"];
+  // console.log(token)
+  //토큰을 복호화한 후 유저를 찾는다
+  User.findByToken(token, (err, user) => {
+    if (err) throw err;
+    if (!user)
+      return res.json({
+        isAuth: false,
+        error: true,
+      });
 
-    //클라이언트 쿠기에서 토큰을 가져온다
-    const token = req.header('x_auth');
-    // console.log(token)
-    //토큰을 복호화한 후 유저를 찾는다
-    User.findByToken(token, (err, user) => {
-        if (err) throw err;
-        if (!user)
-            return res.json({
-                isAuth: false, error: true
-            })
+    req._id = user._id;
+    req.token = token;
+    req.user = user;
+    next();
+  });
+};
 
-        req._id = user._id;
-        req.token = token;
-        req.user = user;
-        next();
-    })
-
-}
-
-module.exports = { auth }
+module.exports = { auth };

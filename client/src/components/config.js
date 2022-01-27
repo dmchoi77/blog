28722770eb444d15f272ae4789d4a0a8b8cc184d
dmchoi <david5760@naver.com>
2022-01-27 21:47:46 +0900

    Fix - headers 수정

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,19 +1,16 @@
-
 //////////////////////////// For Header Configs //////////////////////////////////
 const config = {
-    headers: {
-        'Content-type': 'application/json'
-    }
+  headers: {
+    "Content-type": "application/json;charset=UTF-8",
+    // "Set-Cookie": "HttpOnly,Secure",
+  },
 };
 
-const token = localStorage.getItem('x_auth')
+const token = localStorage.getItem("x_auth");
 
 // If token, add to headers
 if (token) {
-    config.headers['x_auth'] = token;
+  config.headers["x_auth"] = token;
 }
 
-config.headers['Content'] = "application/json;charset=UTF-8";
-
-
-export const headersConfig = config
+export const headersConfig = config;
